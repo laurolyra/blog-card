7652e32cf2edf0ecc61f8f4a4ef8001b9d3a476a
Lauro Lyra Aguiar <laurolyra@gmail.com>
2026-03-23 12:38:18 -0300

    formatted app; component on center

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import './App.css'
-import BlogCard from './components/BlogCard'
-import Credits from './components/Credits'
-import spaceJoy from './assets/spacejoy-YqFz7UMm8qE-unsplash.jpg'
+import "./App.css";
+import BlogCard from "./components/BlogCard";
+import Credits from "./components/Credits";
+import spaceJoy from "./assets/spacejoy-YqFz7UMm8qE-unsplash.jpg";
 
 function App() {
-
   return (
-    <div className='min-h-screen bg-linear-to-br from-[#F9FAFB] to-[#D2D6DB]'>
-      <div className='flex justify-center'>
+    <div className="min-h-screen bg-linear-to-br from-[#F9FAFB] to-[#D2D6DB]">
+      <div className="flex justify-center items-center min-h-screen">
         <BlogCard
-          imageAlt='living room'
+          imageAlt="living room"
           imageSrc={spaceJoy}
-          title='Top 5 Living Room Inspirations'
-          subtitle='Curated vibrants colors for your living, make it pop & calm in the same time.'
-          tag='Interior'
+          title="Top 5 Living Room Inspirations"
+          subtitle="Curated vibrants colors for your living, make it pop & calm in the same time."
+          tag="Interior"
+          url="http://github.com/laurolyra"
         />
       </div>
-      <div className='fixed bottom-6 left-6'>
+      <div className="fixed bottom-6 left-6">
         <Credits author="Lauro Lyra Aguiar" />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
