f5539c0670bfae82149ace4085ac882d14212aae
Lauro Lyra Aguiar <laurolyra@gmail.com>
2026-03-23 11:41:02 -0300

    base rules defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import BlogCard from './components/BlogCard'
 import Credits from './components/Credits'
+import spaceJoy from './assets/spacejoy-YqFz7UMm8qE-unsplash.jpg'
 
 function App() {
 
   return (
     <div className='min-h-screen bg-linear-to-br from-[#F9FAFB] to-[#D2D6DB]'>
       <div className='flex justify-center'>
-        <BlogCard />
+        <BlogCard imageAlt='living room' imageSrc={spaceJoy}/>
       </div>
       <div className='fixed bottom-6 left-6'>
         <Credits author="Lauro Lyra Aguiar" />
--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,7 +1,27 @@
-export default function BlogCard() {
+interface BlogCardProps {
+    imageAlt: string;
+    imageSrc: string;
+}
+
+export default function BlogCard({imageAlt, imageSrc}: BlogCardProps) {
+    
     return (
-        <div className="border rounded-lg">
-            <h1>blog card</h1>
+        <div className="w-85 h-129.5 font-sans rounded-lg">
+            <img
+            alt={imageAlt}
+            src={imageSrc}
+            className="rounded-t-lg"
+            />
+            <div className="p-4 pb-6 flex flex-col justify-between bg-white rounded-b-lg shadow-md">
+                <div className="py-0.5 px-2 bg-[#F0FDF4] border border-[#BBF7D0] rounded-2xl w-fit">
+                    <p className="text-[#15803D] text-sm">Interior</p>
+                </div>
+                    <h1 className="font-bold text-lg">Top 5 Living room Inspirations</h1>
+                <div>
+                    <h2 className="text-[#525252] text-base">Curated vibrants colors for your living, make it pop & calm in the same time.</h2>
+                    <p className="text-[#4338CA] text-base">Read more</p>
+                </div>
+            </div>
         </div>
     )
 
